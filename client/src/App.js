0363057463a877df24920134218b35550bceaaba
[Google Jr] <andilembele020@gmail.com>
2020-08-24 19:17:53 +0200

    Fixed the troublesome error

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
+import {gql} from '@apollo/client';
+import { graphql } from '@apollo/client/react/hoc';
+// import { Query, Mutation, Subscription } from '@apollo/client/react/components';
+
+const TodosQuery = gql`
+  {
+    todos {
+        id
+        text
+        complete
+    }
+  }
+`;
 
 class App extends Component {
     render() {
+      const { data: { loading, todos}} = this.props;
+
+      if (loading) {
+        return null;
+      }
+
         return (
             <div>
-                Hello
+                {todos.map(todo => <div key={`${todo.id}=todo-item`}>{todo.text}</div>)}
             </div>
         );
     }
 }
 
-export default App;
+export default graphql(TodosQuery) (App);
+
--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+  ApolloClient,
+  InMemoryCache,
+  // eslint-disable-next-line
+  NormalizedCacheObject,
+  ApolloProvider
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+});
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
